32049884fd3b00a6f82f9dad6ebaa3dec0b148ee
John Hui <mjhui90@gmail.com>
2019-06-30 21:51:41 -0400

    Deleted console log

--- server/api/portfolio.js
+++ server/api/portfolio.js
@@ -4,9 +4,7 @@ const getPortfolioAndHoldings = require('../db/queries')
 
 router.get('/', async (req, res) => {
   const {email} = req.query
-
   const userData = await getPortfolioAndHoldings(email)
-  //  console.log(userData.portfolio.holdings)
   res.json(userData)
 })
 
